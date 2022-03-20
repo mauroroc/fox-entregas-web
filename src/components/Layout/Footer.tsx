1747fb06a5c2aac7c96c43afa1226d50f5899e3e
Mauro Rocha <mauro.rocha@al.infnet.edu.br>
2022-03-19 21:12:06 -0300

    Footer

---
+++ src/components/Layout/Footer.tsx
@@ -0,0 +1,45 @@
+import { Container, Nav } from "react-bootstrap"
+import LogoWhite from "../../assets/img/logo-fox-entregas-white.svg"
+import styled from "styled-components"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faFacebookSquare, faInstagram } from "@fortawesome/free-brands-svg-icons"
+
+export default function Footer() {
+  return (
+    <FooterStyled className="text-center pt-4 pb-5 py-lg-3">
+      <Container className="d-lg-flex align-items-center">
+        <a href="/">
+          <LogoStyled src={LogoWhite} alt="Logo Fox Entregas" width={255} height={67} />
+        </a>
+        <Nav className="flex-column flex-lg-row my-4 my-lg-0 ms-auto">
+          <Nav.Link href="/" className="text-white">Inicio</Nav.Link>
+          <Nav.Link href="/cadastro" className="text-white">Cadastro</Nav.Link>
+          <Nav.Link href="/login" className="text-white">Login</Nav.Link>
+          <Nav.Link href="/termos-de-uso.pdf" target="_blank" className="text-white">Termos de Uso</Nav.Link>
+        </Nav>
+        <Nav className="justify-content-center">
+          <Nav.Link href="https://facebook.com" target="_blank" rel='noreferrer noopener' className="py-0 px-2">
+            <IconStyled icon={faFacebookSquare} className="text-white"/>  
+          </Nav.Link>
+          <Nav.Link href="https://instagram.com" target="_blank" rel='noreferrer noopener' className="py-0 px-2">
+            <IconStyled icon={faInstagram} className="text-white"/>  
+          </Nav.Link>
+        </Nav>
+      </Container>
+    </FooterStyled>
+  )
+}
+
+const LogoStyled = styled.img`
+  @media(min-width: 992px) {
+    width: 190px;    
+  }
+`
+
+const FooterStyled = styled.footer`
+  background-color: #414141;
+`
+
+const IconStyled = styled(FontAwesomeIcon)`
+  font-size: 2.5rem;
+`
