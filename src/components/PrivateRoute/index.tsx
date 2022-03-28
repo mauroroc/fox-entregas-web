66f38fafd1e283acbf546bad77751ea2f365c03d
Mauro Rocha <mauro.rocha@al.infnet.edu.br>
2022-03-28 20:40:51 -0300

    Iniciando cadastro

---
+++ src/components/PrivateRoute/index.tsx
@@ -0,0 +1,17 @@
+import { Children } from "react"
+import { useSelector } from "react-redux"
+import { Navigate } from "react-router-dom"
+import { selectIsLoadingUser, selectIsUserLoggedIn } from "../../store/slices/userSlices"
+import Loading from "../Loading"
+
+type Props = {
+  children: React.ReactElement
+}
+
+export default function PrivateRoute({ children }: Props) {
+  const isLoadingUser = useSelector(selectIsLoadingUser)
+  const isUserLoggedIn = useSelector(selectIsUserLoggedIn)
+  if (isLoadingUser) return <Loading />
+  if (!isUserLoggedIn) return <Navigate to="/login" />  
+  return children
+}
--- src/routes.tsx
+++ src/routes.tsx
@@ -2,9 +2,11 @@ import {
   Routes as RoutesDOM,
   Route,
 } from "react-router-dom";
+import PrivateRoute from "./components/PrivateRoute";
 import PublicOnlyRoute from "./components/PublicOnlyRoute";
 import HomeView from "./views/Home";
 import LoginView from "./views/Login";
+import NewOrderView from "./views/NewOrder";
 import { NotFoundView } from "./views/NotFound";
 import { RegisterView } from "./views/Register";
 
@@ -21,6 +23,11 @@ export default function Routes() {
         <PublicOnlyRoute>
           <LoginView />
         </PublicOnlyRoute>
+        } />
+        <Route path="/novo-pedido" element={  
+          <PrivateRoute>
+            <NewOrderView />        
+          </PrivateRoute>     
         } />
       <Route path="*" element={<NotFoundView />} />
     </RoutesDOM>
