6f10fb21ff5e7c1605b107eb492ad2eea2a37cd4
Mauro Rocha <mauro.rocha@al.infnet.edu.br>
2022-03-27 21:41:14 -0300

    identifica usuario logado

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,20 @@ import { useEffect } from "react";
 import Routes from "./routes";
 import { onAuthStateChanged } from 'firebase/auth'
 import { auth } from "./services/firebase";
+import { getUser } from "./services/getUser";
+import { useDispatch } from "react-redux";
+import { updateUser } from "./store/slices/userSlices";
 
 function App() {
+  const dispatch = useDispatch()
   useEffect(()=> {
-    onAuthStateChanged( auth, (currentUser)=> {
-      console.log('user:', currentUser)
+    onAuthStateChanged( auth, async (currentUser)=> {
+      if (currentUser) {
+        const user = await getUser(currentUser.uid)        
+        dispatch(updateUser(user))
+      }
     })
-  },[])
-
+  },[dispatch])
   return (
     <Routes />
   );
